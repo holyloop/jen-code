4336d27d1648620f932c02b778c081a94942eb2f
holyloop <holyloop@163.com>
2018-08-08 20:48:35 +0800

    add aop-styled transaction control

---
+++ src/back/src/main/java/com/github/holyloop/jencode/config/TransacationConfig.java
@@ -0,0 +1,64 @@
+package com.github.holyloop.jencode.config;
+
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.aop.Advisor;
+import org.springframework.aop.aspectj.AspectJExpressionPointcut;
+import org.springframework.aop.support.DefaultPointcutAdvisor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.TransactionDefinition;
+import org.springframework.transaction.interceptor.*;
+
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+
+@Aspect
+@Configuration
+public class TransacationConfig {
+
+    private static final int TX_METHOD_TIMEOUT = 60;
+
+    private static final String AOP_POINTCUT_EXPRESSION = "execution (* com.github.holyloop.jencode.service.*.*(..))";
+
+    @Autowired
+    private PlatformTransactionManager transactionManager;
+
+    @Bean
+    public TransactionInterceptor txAdvice() {
+        NameMatchTransactionAttributeSource source = new NameMatchTransactionAttributeSource();
+
+        RuleBasedTransactionAttribute readOnlyTx = new RuleBasedTransactionAttribute();
+        readOnlyTx.setReadOnly(true);
+        readOnlyTx.setPropagationBehavior(TransactionDefinition.PROPAGATION_NOT_SUPPORTED);
+
+        RuleBasedTransactionAttribute requiredTx = new RuleBasedTransactionAttribute();
+        requiredTx.setRollbackRules(Collections.singletonList(new RollbackRuleAttribute(Exception.class)));
+        requiredTx.setPropagationBehavior(TransactionDefinition.PROPAGATION_REQUIRED);
+        requiredTx.setTimeout(TX_METHOD_TIMEOUT);
+
+        Map<String, TransactionAttribute> txMap = new HashMap<>();
+        txMap.put("select*", readOnlyTx);
+        txMap.put("add*", requiredTx);
+        txMap.put("save*", requiredTx);
+        txMap.put("insert*", requiredTx);
+        txMap.put("update*", requiredTx);
+        txMap.put("delete*", requiredTx);
+        txMap.put("*", requiredTx);
+
+        source.setNameMap(txMap);
+        TransactionInterceptor txAdvice = new TransactionInterceptor(transactionManager, source);
+
+        return txAdvice;
+    }
+
+    @Bean
+    public Advisor txAdviceAdvisor() {
+        AspectJExpressionPointcut pointcut = new AspectJExpressionPointcut();
+        pointcut.setExpression(AOP_POINTCUT_EXPRESSION);
+        return new DefaultPointcutAdvisor(pointcut, txAdvice());
+    }
+
+}
